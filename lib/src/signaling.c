28898f2bdc80996cd7fddd5b65b60735ffee35ea
Cristian S Tapiero Maldonado <73723208+ctapiero@users.noreply.github.com>
2023-09-27 09:55:00 -0600

    added function devlarations for handler and request

--- lib/src/signaling.c
+++ lib/src/signaling.c
@@ -1,12 +1,12 @@
 void signal_handle_calculation(struct k_sem *request,
                                struct k_sem *response,
                                struct signal_data *data){
-    //TODO                              
+    //TODO:                              
 }
 
 int signal_request_calculate(struct k_sem *request,
                              struct k_sem *response,
                              struct signal_data *data){
 
-    //TODO                            
+    //TODO:                          
 }
